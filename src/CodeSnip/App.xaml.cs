9a75d062b242693e4fa348d84ae3b53d6debabbf
mx7b7 <profic.baltic@gmail.com>
2025-09-21 13:25:36 +0200

    splash screen redesign

--- src/CodeSnip/App.xaml.cs
+++ src/CodeSnip/App.xaml.cs
@@ -14,18 +14,23 @@ namespace CodeSnip
             try
             {
                 var mainWindow = new MainWindow();
-                await PerformInitializationAsync(mainWindow);
+
+                // Run initialization and a minimum delay in parallel
+                Task initializationTask = PerformInitializationAsync(mainWindow);
+                Task delayTask = Task.Delay(2000); // Minimum 2 seconds display time
+                await Task.WhenAll(initializationTask, delayTask);
+
                 MainWindow = mainWindow;
                 mainWindow.Show();
             }
             catch (Exception ex)
             {
                 MessageBox.Show($"A critical error occurred during application startup and the application will now close.\n\nError: {ex.Message}", "Startup Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                Shutdown();
+                Shutdown(1);
             }
             finally
             {
-                splashScreen.Close();
+                await splashScreen.CloseWithFadeOut();
             }
         }
         private async Task PerformInitializationAsync(MainWindow mainWindow)
--- src/CodeSnip/Views/SplashScreenView/SplashScreen.xaml.cs
+++ src/CodeSnip/Views/SplashScreenView/SplashScreen.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using MahApps.Metro.Controls;
+using System.Windows.Media.Animation;
 
 namespace CodeSnip.Views.SplashScreenView
 {
@@ -7,6 +8,21 @@ namespace CodeSnip.Views.SplashScreenView
         public SplashScreen()
         {
             InitializeComponent();
+        }
+        private void SplashScreen_ContentRendered(object? sender, EventArgs e)
+        {
+            var fadeInAnimation = (Storyboard)FindResource("FadeIn");
+            fadeInAnimation.Begin(this);
+        }
+
+        public async Task CloseWithFadeOut()
+        {
+            var fadeOutAnimation = (Storyboard)FindResource("FadeOut");
+            var tcs = new TaskCompletionSource<bool>();
+            fadeOutAnimation.Completed += (s, e) => tcs.SetResult(true);
+            fadeOutAnimation.Begin(this);
+            await tcs.Task;
+            Close();
         }
     }
 }
